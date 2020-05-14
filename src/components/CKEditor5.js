a2c558afcb482e93ea1be9cb8d1ddc682878be46
Jesse Tao <c.tao@griffith.edu.au>
2020-05-14 11:51:40 +1000

    14/5 demo

--- src/components/CKEditor5.js
+++ src/components/CKEditor5.js
@@ -1,5 +1,4 @@
-import React, { Component } from 'react';
-import * as data from './db.json';
+import React, { Component, useState, useEffect } from 'react';
 import './CKEditor5.css';
 import GRIFFITH_LOGO from './assets/Griffith Logo.png';
 import LOCK_ICN from './assets/lock_icn.svg';
@@ -10,161 +9,215 @@ import xIcon from './assets/xIcon.svg';
 import CKEditor from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@jesse541/ckeditor5-build-classic';
 
-class CKeditor5 extends Component {
-    state = {
-        questions: data.questions, 
+const CKeditor5 = (props) => {
+    console.log('this is the props: ', props)
+    const defaultState = {
+        questions: [], 
         currentQuestionId: 0,
         currentData: null,
-        words: 0,
+        currentWords: 0,
         showLockBar: false,
-        showExamplerAnswer: false
+        showExamplerAnswer: false,
+        isLoading: true
+    }
+    const [state, setState] = useState(defaultState);
+
+    useEffect(() => {
+        console.log('useEffect from child ', state.isLoading, state.questions);
+        setState({...state, questions:props.questionSet.questions, isLoading: false})
+    }, [state.isLoading]);
+
+    const clickCloseExampleAnswer = () => {
+        setState({...state, showExamplerAnswer: false})
     };
 
-    nextQuestionId = (way) => {
-        const newID = way === 'up' ? this.state.currentQuestionId - 1 : this.state.currentQuestionId + 1;
-        if (newID >= 0 && newID < this.state.questions.length) {
-            this.setState({
-                currentQuestionId: newID,
-                currentData: '',
-                showExamplerAnswer: false
-            })
-        }
-    }
-    
-    saveAnswer = () => {
-        const currentAnswer = this.state.currentData;
+    const saveAnswer = () => {
+        const currentAnswer = state.currentData;
         if (currentAnswer) {
-            const questions = this.state.questions;
-            questions[this.state.currentQuestionId].answer = currentAnswer;
-            this.setState({
-                questions,     
-            });
-        } else {
-            console.log(`failed. ${currentAnswer}`);
+            const questions = state.questions;
+            questions[state.currentQuestionId].my_responses.push(currentAnswer);
+            setState({...state, questions})
         }
     }
-    clickCloseExampleAnswer = () => {
-        this.setState({showExamplerAnswer: false})
-    }
-    clickShowExamplerAnswer = () => {
-        const answer = this.state.questions[this.state.currentQuestionId].answer;
-        // const examplerAnswer = this.state.questions[this.state.currentQuestionId]["example-answer"];
-        if (answer) {
-            this.setState({showExamplerAnswer: true})
+
+    const clickShowExamplerAnswer = () => {
+        const my_responses = state.questions[state.currentQuestionId].my_responses;
+        if (my_responses) {
+            setState({...state, showExamplerAnswer: true})
         } else {
             console.log('can not show exampler answer')
         }
     }
 
-    render() {
-        console.log(this.state);
-
-        const examplerAnswer = this.state.questions[this.state.currentQuestionId]["example-answer"];
-        const answer = this.state.questions[this.state.currentQuestionId].answer;
-        const examplerAnswerStyle = {
-            display: this.state.showExamplerAnswer ? 'flex' : 'none'
+    const nextQuestionId = (way) => {
+        const newID = way === 'up' ? state.currentQuestionId - 1 : state.currentQuestionId + 1;
+        if (newID >= 0 && newID < state.questions.length) {
+            setState((state) => ({...state, currentQuestionId: newID, currentData: '', showExamplerAnswer: false}))
+            console.log(`current question id is ${defaultState.currentQuestionId}, new question id is ${newID}`)
         }
-        const ckeditorStyle = {
-            display: this.state.showExamplerAnswer ? 'none' : 'block'
-        }
-        const lockBarStyle = {
-            display: (examplerAnswer && !this.state.showExamplerAnswer) ? 'block' : 'none',
-            cursor: answer ? 'pointer' : 'auto'
-        };
+    }
 
-        return (
-            <div className='multiQuestionsView'>
-                <div className='questionHeader'>
-                    <div className='codeOfConduct'>
-                        <h1>Code of Conduct Activities</h1>
-                        <p>Questions for you on the code of conduct</p>
-                    </div>
-                    <div className='griffithLogo'>
-                        <img src={GRIFFITH_LOGO} alt="Griffith Logo" />
-                    </div>
+    // const saveAndContinue = () => {
+    //     const currentAnswer = state.currentData;
+    //     console.log(currentAnswer);
+    //     if (currentAnswer) {
+    //         console.log('saveAnswering...')
+    //         const questions = state.questions;
+    //         questions[state.currentQuestionId].my_responses.push(currentAnswer);
+    //         setState(state => ({...state, questions}))
+    //     }
+    //     const newID = state.currentQuestionId + 1;
+    //     if (newID < state.questions.length) {
+    //         setState(state => ({...state, currentQuestionId: newID, currentData: '', showExamplerAnswer: false}))
+    //         console.log('nextQuestioning..., currentQuestionID is :', state.currentQuestionId)
+    //     }
+    // }
+
+    const updateWrodCount = (stats) => {
+        console.log(stats)
+        const currentWords = stats.words;
+        console.log('words: ', currentWords)
+        setState(state => ({...state, currentWords}))
+    }
+
+    console.log('this is state: ', state);
+
+    if (state.isLoading || state.isLoading === undefined) {
+        console.log('Loading Child page', state.isLoading )
+        return(<p>This page is loading...</p>)
+    }
+    
+    const examplerAnswerStyle = {
+        display: state.showExamplerAnswer ? 'flex' : 'none'
+    };
+
+    const ckeditorStyle = {
+        display: state.showExamplerAnswer ? 'none' : 'block'
+    }
+
+    const examplerAnswer = state.questions[state.currentQuestionId].example_content;
+    const my_responses = state.questions[state.currentQuestionId].my_responses;
+    const lockBarStyle = {
+        display: (examplerAnswer && !state.showExamplerAnswer) ? 'block' : 'none',
+        cursor: my_responses ? 'pointer' : 'auto'
+    };
+
+    // const numberOfQuestionWithResponse = () => {
+    //     // var i;
+    //     // for (i = 0; i < cars.length; i++) {
+    //     // text += cars[i] + "<br>";
+    //     // }
+
+    //     const num = 0;
+    //     var i;
+    //     for (i = 0; i < state.questions.length; i++) {
+    //         if (i / 2 === 0) {
+    //             num = num + 1;
+    //         }
+    //     }
+    //     return num;
+
+    // }
+
+    return (
+        <div className='multiQuestionsView'>
+
+            <div className='questionHeader'>
+                <div className='codeOfConduct'>
+                    <h1>{props.questionSet.title}</h1>
+                    <p>{props.questionSet.description}</p>
                 </div>
-                <div className='questionSection'>
-                    <div className='questions'>
-                        <h1>{this.state.questions[this.state.currentQuestionId].title}</h1>
-                        <p>{this.state.questions[this.state.currentQuestionId].content}</p>
-                    </div>
-                    <div className='examplerAnswer' style={examplerAnswerStyle}>
-                        <img src={xIcon} alt="xIcon" onClick={this.clickCloseExampleAnswer} />
-                        <h1>Exemplar Answer</h1>
-                        <p>{this.state.questions[this.state.currentQuestionId]["example-answer"]}</p>
-                    </div>
-                    <div className='ckeditor' style={ckeditorStyle}>
-                        <CKEditor
-                            editor={ ClassicEditor }
-                            config={{
-                                toolbar: [ 'heading', 'alignment:left', 'alignment:justify', 'alignment:right', '|', 'bold', 'underline', 'italic', '|', 'bulletedList', 'numberedList', '|', 'undo', 'redo' ],
-                                heading: {
-                                    options: [
-                                        { model: 'heading2', view: 'h2', title: 'Heading', class: 'ck-heading_heading2' },
-                                        { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' }
-                                    ]
-                                },
-                                placeholder: "Enter your response here",
-                                wordCount: {
-                                    onUpdate: stats => {
-                                        this.setState({words: stats.words})
-                                    }
+
+                <div className='griffithLogo'>
+                    <img src={GRIFFITH_LOGO} alt="Griffith Logo" />
+                </div>
+            </div>
+
+            <div className='questionSection'>
+                <div className='questions'>
+                    <h1>{state.questions[state.currentQuestionId].title}</h1>
+                    <p>{state.questions[state.currentQuestionId].question_text}</p>
+                </div>
+
+                <div className='examplerAnswer' style={examplerAnswerStyle}>
+                    <img src={xIcon} alt="xIcon" onClick={clickCloseExampleAnswer} />
+                    <h1>Exemplar Answer</h1>
+                    <p>{state.questions[state.currentQuestionId].example_content}</p>
+                </div>
+
+                <div className='ckeditor' style={ckeditorStyle}>
+                    <CKEditor
+                        editor={ ClassicEditor }
+                        config={{
+                            toolbar: [ 'heading', 'alignment:left', 'alignment:justify', 'alignment:right', '|', 'bold', 'underline', 'italic', '|', 'bulletedList', 'numberedList', '|', 'undo', 'redo' ],
+                            heading: {
+                                options: [
+                                    { model: 'heading2', view: 'h2', title: 'Heading', class: 'ck-heading_heading2' },
+                                    { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' }
+                                ]
+                            },
+                            placeholder: "Enter your response here",
+                            wordCount: {
+                                onUpdate: stats => {
+                                    updateWrodCount(stats)
                                 }
-                            }}
-                            data={this.state.currentData}
-                            // onInit={ editor => {
-                            //     // You can store the "editor" and use when it is needed.
-                            //     console.log( 'Editor is ready to use!', editor );
-                            // } }
-                            // onChange={ ( event, editor ) => {
-                            //     this.setState({currentData: editor.getData()});
-                            //     // const data = editor.getData();
-                            //     // console.log( { event, editor, data } );
-                            // } }
-                            onBlur={ ( event, editor ) => {
-                                this.setState({currentData: editor.getData()});
-                            } }
-                            // onFocus={ ( event, editor ) => {
-                            //     console.log( 'Focus.', editor );
-                            // } }
-                        />
-                        <p id='wordCount'>{`${this.state.words} words`}</p>
-                    </div>
-                    <div className='gap'></div>
-                    <div className='lockBar' style={lockBarStyle} onClick={this.clickShowExamplerAnswer} >
-                        <div className='lock'>
-                            <img src={LOCK_ICN} alt="Lock Icon" />
-                            <p>After you have answered the question please view this exemplar answer</p>
-                        </div>
-                    </div>
+                            }
+                        }}
+                        data={state.currentData}
+                        onInit={ editor => {
+
+                        } }
+                        onChange={ ( event, editor ) => {
+                            
+                        } }
+                        onBlur={ ( event, editor ) => {
+                            setState({...state, currentData: editor.getData()});
+                        } }
+                        onFocus={ ( event, editor ) => {
+    
+                        } }
+                    />
+                    <button className='saveButton' onClick={saveAnswer} >
+                        <p>Save</p>
+                    </button>
+                    <p id='wordCount'>{`${state.currentWords} words`}</p>
                 </div>
 
-                <div className='footer'>
+                <div className='gap'></div>
+
+                <div className='lockBar' style={lockBarStyle} onClick={clickShowExamplerAnswer} >
+                    <div className='lock'>
+                        <img src={LOCK_ICN} alt="Lock Icon" />
+                        <p>After you have answered the question please view this exemplar answer</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className='footer'>
                     <div className='progressBar'>
                         <div className='progressBar1'>
-                            <img src={UP_ARROW} alt="Up Arrow" onClick={() => this.nextQuestionId('up')} />
-                            <img src={DOWN_ARROW} alt="Down Arrow" onClick={() => this.nextQuestionId('down')} />
+                            <img src={UP_ARROW} alt="Up Arrow" onClick={() => nextQuestionId('up')} />
+                            <img src={DOWN_ARROW} alt="Down Arrow" onClick={() => nextQuestionId('down')} />
                         </div>
 
                         <div className='progressBar2'>
-                            <p>20% completed (1/5)</p>
+                            <p>{`10% completed (1/${state.questions.length})`}</p>
                             <div className='progress'>
                                 <div className='progress-bar' role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100"></div>
                             </div>
                         </div>
 
                         <div className='progressBar3'>
-                            <button className='returnButton' onClick={this.saveAnswer} >
-                                <p>Save</p>
+                            <button className='saveContinueButton' onClick={() => nextQuestionId('down')} >
+                                <p>Next</p>
                             </button>
                         </div>
                     </div>
                 </div>
-            
-            </div>
-        );
-    }
+
+        </div>
+    );
 }
 
-export default CKeditor5;
 
+export default CKeditor5;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CKeditor5 from './components/CKEditor5';
+import * as questionSet from './questionSet.json';
 
 ReactDOM.render(
-  <CKeditor5 />,
+  <CKeditor5 questionSet={questionSet.data}/>,
   document.getElementById('root')
 );
